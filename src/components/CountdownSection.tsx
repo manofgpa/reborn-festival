3902a63c019918335308d6dbba819364faf80c8a
Felipe Mano <manofgpa@gmail.com>
2021-10-22 19:42:56 -0300

    Add images

---
+++ src/components/CountdownSection.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+
+import {
+  Container,
+  SimpleGrid,
+  Text,
+  Box,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
+export const CountdownSection = () => {
+  const eventDate = "12/31/2021";
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    let difference = +new Date(eventDate) - +new Date();
+    let timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    // Clear timeout if the component is unmounted
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <Container maxW={["xl", "7xl"]}>
+      <Box align="center">
+        <Text
+          as="h2"
+          color="gray.50"
+          fontWeight="bold"
+          fontSize={["2xl", "6xl"]}
+          maxWidth={{ base: "100vh", md: "130vh", lg: "130vh", xl: "130vh" }}
+        >
+          SAVE THE DATE
+        </Text>
+        <Text fontSize={["2xl", "6xl"]} mt={2} as="mark">
+          31/12/2021
+        </Text>
+        <Box color="gray.50" my={8}>
+          {timerComponents.length ? (
+            <SimpleGrid
+              columns={4}
+              spacingX={["10px", "20px"]}
+              spacingY={["0", "20px"]}
+              fontSize={[18, 60]}
+            >
+              <Box height={["20px", "80px"]} justify={"center"}>
+                <Text>{timeLeft?.days}</Text>
+              </Box>
+              <Box height={["20px", "80px"]}>{timeLeft?.hours}</Box>
+              <Box height={["20px", "80px"]}>{timeLeft?.minutes}</Box>
+              <Box height={["20px", "80px"]}>{timeLeft?.seconds}</Box>
+              <Box height={["20px", "80px"]}>Dias</Box>
+              <Box height={["20px", "80px"]}>Horas</Box>
+              <Box height={["20px", "80px"]}>
+                {isWideVersion ? "Minutos" : "Min"}
+              </Box>
+              <Box height={["20px", "80px"]}>
+                {isWideVersion ? "Segundos" : "Seg"}
+              </Box>
+            </SimpleGrid>
+          ) : (
+            <span>Time's up!</span>
+          )}
+        </Box>
+      </Box>
+    </Container>
+  );
+};
---
+++ src/components/TicketsSection.tsx
@@ -0,0 +1,51 @@
+import {
+  Flex,
+  Text,
+  Stack,
+  VStack,
+  useBreakpointValue,
+  Button,
+} from "@chakra-ui/react";
+import { CountdownSection } from "./CountdownSection";
+
+export const TicketsSection = () => {
+  return (
+    <Flex
+      w={"full"}
+      h={"100vh"}
+      bgImage={`linear-gradient(rgba(152, 151, 129, 0.3),rgba(0, 0, 0, 0)),
+        url(/images/tomorrowland2.jpg)`}
+      backgroundSize={"cover"}
+      backgroundPosition={"center center"}
+      // backgroundBlendMode="multiply"
+    >
+      <VStack
+        w={"full"}
+        justify={"center"}
+        px={useBreakpointValue({ base: 4, md: 8 })}
+        bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+      >
+        <Flex
+          direction="column"
+          align="center"
+          bgColor="gray.900"
+          rounded={"xl"}
+          p={[6, 8]}
+        >
+          <CountdownSection />
+          <Button
+            // bg={"#8a561b"}
+            colorScheme="green"
+            rounded={"full"}
+            color={"white"}
+            _hover={{ bg: "#8a561b" }}
+            w={["250px", "500px"]}
+            p={[8, 12]}
+          >
+            GARANTA SEU INGRESSO
+          </Button>
+        </Flex>
+      </VStack>
+    </Flex>
+  );
+};
