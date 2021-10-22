078ee710644a457c03d06e3b744471b2fe5475b4
Felipe Mano <manofgpa@gmail.com>
2021-10-22 08:25:40 -0300

    Initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { HeroSection } from "components/HeroSection";
+
+const Home = () => {
+  return (
+    <Box mb={8} w="full">
+      <HeroSection />
+    </Box>
+  );
+};
+
+export default Home;
