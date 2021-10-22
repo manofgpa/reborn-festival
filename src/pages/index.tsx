0fd7593f2b2e396695676c668af282e42b1b899c
Felipe Mano <manofgpa@gmail.com>
2021-10-22 17:40:14 -0300

    Add tickets section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
+import { CountdownSection } from "components/CountdownSection";
 import FeaturesSection from "components/FeaturesSection";
 import { HeroSection } from "components/HeroSection";
 import { LineupSection } from "components/LinupSection";
 import { LocalSection } from "components/LocalSection";
 import { PartySection } from "components/PartySection";
+import { TicketsSection } from "components/TicketsSection";
 
 const Home = () => {
   return (
@@ -13,6 +15,8 @@ const Home = () => {
       <FeaturesSection />
       <LocalSection />
       <LineupSection />
+      <CountdownSection />
+      <TicketsSection />
     </Box>
   );
 };
