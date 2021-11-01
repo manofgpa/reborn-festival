d9c4623e1e126a7957a47e7c1f84c8310eb30876
Felipe Mano <manofgpa@gmail.com>
2021-11-01 10:28:46 -0300

    InvitedNames: Fix checkout try

--- src/components/Form/InvitedNames.tsx
+++ src/components/Form/InvitedNames.tsx
@@ -56,12 +56,12 @@ export const InvitedNames = ({ quantity, userData }) => {
           quantidade_ingressos: userData.quantity,
         },
       });
+
+      const participants_names = Object.values(data);
+      await checkout({ ...userData, participants_names });
     } catch (error) {
       console.log(error);
     }
-    const participants_names = Object.values(data);
-
-    await checkout({ ...userData, participants_names });
   };
 
   return (
